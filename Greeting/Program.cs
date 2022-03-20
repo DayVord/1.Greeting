0015bf5a332413542ae9bdc761e343d56391f230
DayVord <DayVord@DAYVORD>
2022-03-20 23:17:59 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ Greeting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            if(name != "") Console.WriteLine($"Konichiwa Uwu, {name}");
+            else Console.WriteLine("You had to enter a name...");
+            Console.Read();
+        }
+    }
+}
